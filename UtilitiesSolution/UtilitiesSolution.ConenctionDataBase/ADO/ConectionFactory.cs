112a2990ae0ee818e71c80ca5b275172ccea7b17
Wladigley Simão (Digley) <wladigley@gmail.com>
2016-11-06 18:16:07 -0300

    06-11-2016-Arquivos Iniciais

---
+++ UtilitiesSolution/UtilitiesSolution.ConenctionDataBase/ADO/ConectionFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace UtilitiesSolution.ConenctionDataBase.ADO
+{
+    
+   public class ConectionFactory
+    {
+        static SqlConnection ConnectionDataBase;
+
+        public static SqlConnection BuildConnection()
+        {
+            if (ConnectionDataBase == null)
+            {
+                ConnectionDataBase = new SqlConnection();
+                ConnectionDataBase.ConnectionString = ConfigurationManager.ConnectionStrings["DataBaseContext"].ConnectionString;
+            }
+            if (ConnectionDataBase.State == ConnectionState.Closed)
+                ConnectionDataBase.Open();
+            
+            return ConnectionDataBase;
+        }
+    }
+
+}
